aaf91a4a90350ed83a47578a095bf1eef66215e1
joesimpson <1324811+joesimpson@users.noreply.github.com>
2023-09-21 16:46:12 +0200

    Fix source copyright

--- wolfandsheeps.action.php
+++ wolfandsheeps.action.php
@@ -2,7 +2,7 @@
 /**
  *------
  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
- * WolfAndSheeps implementation : © <Your name here> <Your email address here>
+ * WolfAndSheeps implementation : © joesimpson <1324811+joesimpson@users.noreply.github.com>
  *
  * This code has been produced on the BGA studio platform for use on https://boardgamearena.com.
  * See http://en.doc.boardgamearena.com/Studio for more information.
